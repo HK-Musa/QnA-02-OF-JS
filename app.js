6893d2338d4ca3ed2f9bf4186407075ecc618015
HK-Musa <khatoonhajira71@gmail.com>
2024-10-21 19:27:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,295 @@
+//********Chapter 6-9**  Assignment 5 *****   Task 1 **********
+
+// var val1=prompt("","Enter Value of 'a'")
+// document.write("Result:<br>The value of 'a' is: "+val1+"<br>...................<br><br>The value of ++a is: "+(++val1)+"<br>Now the value of 'a' is: "+val1+"<br><br>The value of a++ is: "+(val1++)+"<br>Now the value of 'a' is: "+val1+"<br><br>The value of --a is: "+(--val1)+"<br>Now the value of 'a' is: "+val1+"<br><br>The value of a-- is: "+(val1--)+"<br>Now the value of 'a' is: "+val1);
+
+//********Chapter 6-9**  Assignment 5 *****   Task 2 **********
+
+// var a=2,b=1;
+// // var result=--a - --b + ++b + b--;
+// document.write("Initially a=2 and b=1");
+// // --a;
+// document.write("<br>--a is "+(--a));
+// // --b;
+// document.write("<br>--b  is "+(--b));
+// // ++b;
+// document.write("<br>++b is "+(++b));
+// result=a+b+b;
+//  document.write("<br>result is "+result);
+
+//********Chapter 6-9**  Assignment 5 *****   Task 3 **********
+
+// var name=prompt("","Enter Your Name");
+// alert("Welcome "+name);
+
+//********Chapter 6-9**  Assignment 5 *****   Task 4 **********
+
+                //////      Not Mention
+
+//********Chapter 6-9**  Assignment 5 *****   Task 5 **********
+
+// var taBle=prompt("","null");
+// if(taBle!=="null")
+// {
+//     document.write("Table of "+taBle);
+//     for(i=1;i<=10;i++)
+//     {
+//         document.write("<br>"+taBle+" * "+i+" = "+(i*taBle));
+
+//     }
+// }
+// if(taBle==="null")
+// {
+//     var a=5;
+//     document.write("Table of "+a);
+//     for(i=1;i<=10;i++)
+//     {
+//         document.write("<br>"+a+" * "+i+" = "+(i*a));
+//     }
+// }
+        ////question hum khali prompt ko condition me kesy use kren.
+//********Chapter 6-9**  Assignment 5 *****   Task 6 **********
+
+// var sub1=prompt("","Enter Name Subject 1");
+// var marks1=prompt("","Enter Obtained Marks of subject 1");
+// var sub2=prompt("","Enter Name Subject 2");
+// var marks2=prompt("","Enter Obtained Marks of subject 2");
+// var sub3=prompt("","Enter Name Subject 3");
+// var marks3=prompt("","Enter Obtained Marks of subject 3");
+// var totalMarks=100;
+// document.write("Subject Total Marks Obtained Marks Percentage <br>"+sub1+totalMarks+" "+" "+" "+marks1+" "+" "+((marks1/totalMarks)*100)+"%"+"<br>"+sub2+totalMarks+" "+" "+" "+marks2+" "+" "+((marks2/totalMarks)*100)+"%"+"<br>"+sub3+totalMarks+" "+" "+" "+marks3+" "+" "+((marks3/totalMarks)*100)+"%");
+
+//********Chapter 9-11**     Assignment 5 *****   Task 1 **********
+
+// var city=prompt("","Enter City Name ");
+// if(city==="Karachi" || city==="karachi")
+// {
+//     document.write("Welcome to City of Lights");
+// }
+// else if(city==="Lahore" || city==="lahore")
+// {
+//     document.write("Welcome to City of Pindi Boys");
+// }
+// else if(city==="Islamabad" || city==="islamabad")
+// {
+//     document.write("Welcome to City of Capital");
+// }
+// else if(city==="Quatta" || city==="quatta")
+// {
+//     document.write("Welcome to City of Gladiators");
+// }
+//********Chapter 9-11**     Assignment 5 *****   Task 2 **********
+
+// var gender=prompt("","Enter Gender ");
+// if(gender==="Male" || gender==="male")
+// {
+//     document.write("Good Morning Sir!");
+// }
+// else if(gender==="Female" || gender==="female")
+// {
+//     document.write("Good Morning Ma'am");
+// }
+// else if(gender!=="Female" || gender!=="male" || gender!=="female" || gender!=="Male")
+// {
+//     document.write("Your are Special!");
+// }
+
+
+//********Chapter 9-11**     Assignment 5 *****   Task 3 **********
+
+// var color=prompt("","Enter Color ");
+// if(color==="red" || color==="Red")
+// {
+//     document.write("Must Stop!");
+// }
+// else if(color==="yellow" || color==="Yellow")
+// {
+//     document.write("Ready to move!");
+// }
+// else if(color==="Green" || color==="green")
+// {
+//     document.write("Move Now!");
+// }
+// else if(color!=="Green" || color!=="green" || color!=="yellow" || color!=="Yellow" || color!=="red" || color!=="Red")
+// {
+//     document.write("Invalid color!");
+// }
+//********Chapter 9-11**     Assignment 5 *****   Task 4 **********
+
+// var fuel=prompt("","Enter Remaining Fuel");
+// if(fuel<0.25){
+//     alert("Please refil the fuel in your car");
+// }else{
+//     alert("Keep Driving!");
+// }
+
+//********Chapter 9-11**     Assignment 5 *****   Task 5  part 'a' **********
+
+//// var a = 4;
+//// if (++a === 5)
+//// {
+////     alert("given condition for variable a is true");
+//// }
+
+    //  alert("given condition for variable a is true because pre increment statisfied the condition");
+
+
+
+//********Chapter 9-11**     Assignment 5 *****   Task 5  part 'b' **********
+
+//// var b = 82;
+////if (b++ === 83){
+////     alert("given condition for variable b is true");
+//// }
+
+    //  alert("given condition for variable b is False because  the condition is not statisfied");
+
+//********Chapter 9-11**     Assignment 5 *****   Task 5  part 'c' **********
+
+//// var c = 12;
+//// if(c++ === 13){
+////     alert("condition 1 is true");
+//// }if(c === 13){
+////     alert("condition 2 is true");
+//// }if(++c < 14){
+////     alert("condition 3 is true");
+//// }if(c === 14){
+////     alert("condition 4 is true");
+//// }
+
+    //  alert("Condition 2 is true because  the post increment in condition 1  'C' will become 13");
+
+//********Chapter 9-11**     Assignment 5 *****   Task 5  part 'd' **********
+
+//// var materialCost = 20000;
+//// var laborCost = 2000;
+//// var totalCost = materialCost + laborCost;
+//// if (totalCost === laborCost + materialCost){
+////      alert("The cost equals");
+////     }
+
+    //  alert("Because Condition satisfied");
+
+
+//********Chapter 9-11**     Assignment 5 *****   Task 5  part 'e' **********
+
+//// if(true){
+////     alert("True");
+//// } if (false){
+////     alert("False");
+//// }
+
+    //  alert("True Because by default it contain true higher possible value");
+
+//********Chapter 9-11**     Assignment 5 *****   Task 5  part 'f' **********
+
+//// if("car" > "cat"){
+////     alert("car is smaller than cat");
+//// }
+
+    //  alert("In such if condition it check out the index value of each letter and compare them");
+
+//********Chapter 9-11**     Assignment 5 *****   Task 6 **********
+
+// var marks1=+prompt("","Enter Obtained Marks of subject 1");
+// var marks2=+prompt("","Enter Obtained Marks of subject 2");
+// var marks3=+prompt("","Enter Obtained Marks of subject 3");
+// var totalMarks=100;
+// var percenTage=((marks1+marks2+marks3)/(totalMarks*3))*100;
+// if(80<=percenTage){
+//     document.write("<h1>Marks Sheet</h1><br>Total marks: "+(totalMarks*3)+"<br>Marks obtained: "+(marks1+marks2+marks3)+"<br>Percentage: "+percenTage+"%<br>Grade: A-one <br>Remarks: Excellent")
+// }
+// else if(70<=percenTage && percenTage<80){
+//     document.write("<h1>Marks Sheet</h1><br>Total marks: "+(totalMarks*3)+"<br>Marks obtained: "+(marks1+marks2+marks3)+"<br>Percentage: "+percenTage+"%<br>Grade: A <br>Remarks: Good")
+// }
+// else if(60<=percenTage && percenTage<70){
+//     document.write("<h1>Marks Sheet</h1><br>Total marks: "+(totalMarks*3)+"<br>Marks obtained: "+(marks1+marks2+marks3)+"<br>Percentage: "+percenTage+"%<br>Grade: B <br>Remarks: You need to improve")
+// }
+// else if(60>percenTage && percenTage===0){
+//     document.write("<h1>Marks Sheet</h1><br>Total marks: "+(totalMarks*3)+"<br>Marks obtained: "+(marks1+marks2+marks3)+"<br>Percentage: "+percenTage+"%<br>Grade: Fail <br>Remarks: Sorry")
+// }
+
+//********Chapter 9-11**     Assignment 5 *****   Task 7 **********
+
+// var num=5;
+// var guest=+prompt("","Enter a number between 0-10");
+// if(guest===num){
+//     alert(" Bingo! Correct answer.");
+// }
+// else if(guest>num){
+//     alert(" Close enough but you May be come down.");
+// }
+// else if(guest<num){
+//     alert(" Close enough but you May be come up.");
+// }
+
+//********Chapter 9-11**     Assignment 5 *****   Task 8 **********
+
+// var val=+prompt("","Enter a value to check divisible by '3'");
+// if(val%3===0){
+//     alert("Yes! "+val+" is divisible by 3");
+// }
+// else{
+//     alert("No! "+val+" is not divisible by 3");
+// }
+
+//********Chapter 9-11**     Assignment 5 *****   Task 9 **********
+
+// var val=+prompt("","Enter a value to check Even or Odd");
+// if(val%2===0){
+//     alert("Yes! "+val+" is Even Number");
+// }
+// else{
+//     alert("Yes! "+val+" is Odd Number");
+// }
+
+//********Chapter 9-11**     Assignment 5 *****   Task 10 **********
+
+// var temp=+prompt("","Enter Temperature Value");
+// if(40<=temp){
+//     alert("It is too Hot outside.")
+// }
+// else if(30<=temp && temp<40){
+//     alert("The Weather today is Normal.")
+// }
+// else if(20<=temp && temp<30){
+//     alert("Today's Weather is Cool.")
+// }
+// else if(10<=temp && temp<20){
+//     alert("OMG! Today's weather is so Cool.")
+// }
+// else if(10>temp){
+//     alert("Stay Home Stay Save !")
+// }
+
+//********Chapter 9-11**     Assignment 5 *****   Task 11 **********
+
+// var a=+prompt("","Enter First Value");
+// var operator=prompt("","Enter Operator to Perform + - * / %")
+// var b=+prompt("","Enter Second Value");
+// if(operator === "-")
+//     {
+//         var c=a-b;
+//         document.write("Difference of "+a+" and "+b+" is "+c+".");
+//     }
+// else if(operator === "*")
+//     {
+//         var c=a*b;
+//         document.write("Multiple of "+a+" and "+b+" is "+c+".");
+//     }
+// else if(operator === "/")
+//     {
+//         var c=a/b;
+//         document.write("Division of "+a+" by "+b+" is "+c+".");
+//     }
+// else if(operator === "+")
+//     {
+//         var c=a+b;
+//         document.write("Sum of "+a+" and "+b+" is "+c+".");
+//     }
+// else if(operator === "%")
+//     {
+//         var c=b/a;
+//         document.write("Modulus of "+a+" and "+b+" is "+c*100+"% .");
+//     }
+
